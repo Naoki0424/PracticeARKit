ae73560aa2cca40a35493e6c9940a9c05d8bf737
Naoki Tsunoda <Tn04240709@icloud.com>
2021-03-11 22:30:50 +0900

    【add】ピエよん完了（低クオリティ）

--- DetectFacialexpressions/VirtualContentController/DetectFacePieyonController.swift
+++ DetectFacialexpressions/VirtualContentController/DetectFacePieyonController.swift
@@ -10,6 +10,9 @@ import RealityKit
 
 class DetectFacePieyonController: NSObject, VirtualContentController {
     
+    private var originalMouthTopZ: Float = 0
+    private var originalMouthUnderZ: Float = 0
+    
     func updateObject(_ session: ARSession,_ anchors: [ARAnchor],_ parentAnchorEntity : Entity) {
         for anchor in anchors {
             guard let faceAnchor = anchor as? ARFaceAnchor
@@ -17,23 +20,23 @@ class DetectFacePieyonController: NSObject, VirtualContentController {
             
             let blendShapes = faceAnchor.blendShapes
             
+            // これしか思いつかなかった。。。
+            if originalMouthTopZ == 0 {
+                originalMouthTopZ = 0.02699686
+                originalMouthUnderZ = 0.053223185
+            }
+            
             guard let eyeBlinkLeft = blendShapes[.eyeBlinkLeft] as? Float,
                 let eyeBlinkRight = blendShapes[.eyeBlinkRight] as? Float,
-                let mouthRollUpper = blendShapes[.mouthRollUpper] as? Float,
-                let mouthRollLower = blendShapes[.mouthRollLower] as? Float
+                let mouthFunnel = blendShapes[.mouthFunnel] as? Float
                 else { return }
             
-            print("eyeBlinkLeft:", eyeBlinkLeft)
-            print("eyeBlinkRight:", eyeBlinkRight)
-            print("mouthRollUpper:", mouthRollUpper)
-            print("mouthRollLower:", mouthRollLower)
+            pieyonObject.eyeLeft!.scale.y = (1 - eyeBlinkLeft) * 0.2
+            pieyonObject.eyeRight!.scale.y = (1 - eyeBlinkRight) * 0.2
             
+            pieyonObject.mouthTop!.position.z = (originalMouthTopZ - (pieyonObject.mouthTop!.position.z * mouthFunnel))
+            pieyonObject.mouthUnder!.position.z = (originalMouthUnderZ + (pieyonObject.mouthUnder!.position.z * mouthFunnel))
             
-//            pieyonObject.eyeLeft
-//            pieyonObject.eyeRight
-//
-//            pieyonObject.mouthRollUpper
-//            pieyonObject.mouthRollLower
             
             parentAnchorEntity.addChild(pieyonObject)
             
